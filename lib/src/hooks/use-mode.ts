468e867e993dc5c7d6dc13cf28e51ca6d0502ba0
tonyandy5630 <tonyandy456@gmail.com>
2024-11-01 15:56:17 +0700

    <feat> handle sync state between tabs

--- lib/src/hooks/use-mode.ts
+++ lib/src/hooks/use-mode.ts
@@ -1,4 +1,5 @@
-import { ColorSchemePref, ResolvedScheme, useStore } from "../utils";
+import { useEffect } from "react";
+import { ColorSchemePref, parseState, ResolvedScheme, stateKey, useStore } from "../utils";
 
 export interface UseModeYield {
   mode: ColorSchemePref;
@@ -18,11 +19,35 @@ export interface UseModeYield {
  */
 
 export const useMode = (): UseModeYield => {
-  const [{ mode, systemMode }, setState] = useStore();
+  const [{ mode, systemMode }, setThemeState] = useStore();
 
   const setMode = (mode: ColorSchemePref) => {
-    setState(state => ({ ...state, mode }));
+    setThemeState(state => ({ ...state, mode }));
   };
+
+  useEffect(() => {
+    const media = window.matchMedia("(prefers-color-scheme: dark)");
+    const updateSystemColorScheme = () => {
+      setThemeState(state => ({ ...state, systemMode: media.matches ? "dark" : "light" }));
+    };
+
+    updateSystemColorScheme();
+    //* listen for changes in system color scheme
+    media.addEventListener("change", updateSystemColorScheme);
+    //* syncing state between tabs using local storage
+    setThemeState(state => ({ ...state, ...parseState(localStorage.getItem(stateKey)) }));
+    const storageListener = (e: StorageEvent) => {
+      if (e.key === stateKey) {
+        setThemeState(state => ({ ...state, ...parseState(e.newValue) }));
+      }
+    };
+    addEventListener("storage", storageListener);
+
+    return () => {
+      media.removeEventListener("change", updateSystemColorScheme);
+      removeEventListener("storage", storageListener);
+    };
+  }, []);
 
   return { mode, systemMode, setMode };
 };
--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -13,4 +13,10 @@ const INIT_STORE: Store = {
   systemMode: "light",
 };
 
+export const stateKey = "theme-key-state";
+
+export const parseState = (state: string | null) => {
+  return state ? JSON.parse(state) : INIT_STORE;
+};
+
 export const useStore = () => useRGS<Store>("tony", INIT_STORE);
